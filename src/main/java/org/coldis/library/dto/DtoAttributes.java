1af45586ddba31e38fff9ccbaaf28aca601a8892
rvcoutinho <me@rvcoutinho.com>
2019-03-12 11:58:13 -0300

    - Making utility methods static;
    Signed-off-by: workspace <workspace@workspace>

--- src/main/java/org/coldis/library/dto/DtoAttributes.java
+++ src/main/java/org/coldis/library/dto/DtoAttributes.java
@@ -1,6 +1,6 @@
 package org.coldis.library.dto;
 
-import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
  * DTO attributes metadata.
  */
 @Documented
-@Target(TYPE)
+@Target(METHOD)
 @Retention(RUNTIME)
 public @interface DtoAttributes {
 
