1f5249b6ada4b73a3c6b2c62f9ac620abaf5b34f
workspace <workspace@workspace>
2019-05-05 20:04:07 -0300

    - Creating project;
    Signed-off-by: workspace <workspace@workspace>

--- src/main/java/org/coldis/library/pdf/PdfHelper.java
+++ src/main/java/org/coldis/library/pdf/PdfHelper.java
@@ -1,6 +1,8 @@
 package org.coldis.library.pdf;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.coldis.library.exception.IntegrationException;
 import org.coldis.library.model.SimpleMessage;
@@ -17,26 +19,43 @@ public class PdfHelper {
 	/**
 	 * Reads the text from a PDF.
 	 *
-	 * @param  pdfReader            The PDF reader.
-	 * @param strategy Strategy to read the text from the PDF.
-	 * @return                      The text from the PDF.
-	 * @throws IntegrationException If there is a problem reading the PDF.
+	 * @param  pdfReader               The PDF reader.
+	 * @param  strategy                Strategy to read the text from the PDF.
+	 * @param  preventDuplicateContent If duplicate content should be ignored.
+	 * @return                         The text from the PDF.
+	 * @throws IntegrationException    If there is a problem reading the PDF.
 	 */
-	public static String readPdfText(final PdfReader pdfReader,final TextExtractionStrategy strategy) throws IntegrationException {
-		// PDF text.
-		final StringBuffer pdfText = new StringBuffer();
+	public static String readPdfText(final PdfReader pdfReader, final TextExtractionStrategy strategy,
+			final Boolean preventDuplicateContent) throws IntegrationException {
+		// PDF pages content.
+		final List<String> pdfPagesText = new ArrayList<>();
 		// Tries to get the PDF text.
 		try {
 			// For each PDF page.
 			for (Integer currentPdfPage = 1; currentPdfPage <= pdfReader.getNumberOfPages(); currentPdfPage++) {
-				// Appends the current PDF page text to the complete PDF text.
-				pdfText.append(PdfTextExtractor.getTextFromPage(pdfReader, currentPdfPage,strategy));
+				// Gets the page text.
+				final String pdfPageText = PdfTextExtractor.getTextFromPage(pdfReader, currentPdfPage, strategy);
+				// If the page text repeats the last page.
+				if (preventDuplicateContent && !pdfPagesText.isEmpty()
+						&& pdfPageText.startsWith(pdfPagesText.get(pdfPagesText.size() - 1))) {
+					// Removes the last added page text.
+					pdfPagesText.remove(pdfPagesText.size() - 1);
+				}
+				// Adds the current page text information to the pages text.
+				pdfPagesText.add(pdfPageText);
 			}
 		}
 		// If the text cannot be retrieved.
 		catch (final Exception exception) {
 			// Throws an PDF read exception.
 			throw new IntegrationException(new SimpleMessage("pdf.read.error"), exception);
+		}
+		// PDF text.
+		final StringBuffer pdfText = new StringBuffer();
+		// For each PDF page text.
+		for (final String pdfPageText : pdfPagesText) {
+			// Appends the current PDF page text to the complete PDF text.
+			pdfText.append(pdfPageText);
 		}
 		// Returns the PDF text.
 		return pdfText.toString();
@@ -45,13 +64,14 @@ public class PdfHelper {
 	/**
 	 * Reads the text from a PDF.
 	 *
-	 * @param  pdfContent           The PDF byte content.
-	 * @param  strategy             Strategy to read the text from the PDF.
-	 * @return                      The text from the PDF.
-	 * @throws IntegrationException If there is a problem reading the PDF.
+	 * @param  pdfContent              The PDF byte content.
+	 * @param  strategy                Strategy to read the text from the PDF.
+	 * @param  preventDuplicateContent If duplicate content should be ignored.
+	 * @return                         The text from the PDF.
+	 * @throws IntegrationException    If there is a problem reading the PDF.
 	 */
-	public static String readPdfText(final byte[] pdfContent, final TextExtractionStrategy strategy)
-			throws IntegrationException {
+	public static String readPdfText(final byte[] pdfContent, final TextExtractionStrategy strategy,
+			final Boolean preventDuplicateContent) throws IntegrationException {
 		// PDF reader.
 		PdfReader pdfReader = null;
 		// Tries to read the PDF.
@@ -59,7 +79,7 @@ public class PdfHelper {
 			// Creates the reader for the PDF.
 			pdfReader = new PdfReader(pdfContent);
 			// Reads and returns the PDF text.
-			return PdfHelper.readPdfText(pdfReader, strategy);
+			return PdfHelper.readPdfText(pdfReader, strategy, preventDuplicateContent);
 		}
 		// If the PDF cannot be read.
 		catch (final IOException exception) {
